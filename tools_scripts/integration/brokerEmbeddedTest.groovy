a1c1e6e7b1751111db6300d46853d1be25e38d24
Michael Mair <michael.mair@leftshift.one>
2019-03-24 23:28:47 +0100

    Set version number to snapshot

--- tools_scripts/integration/brokerEmbeddedTest.groovy
+++ tools_scripts/integration/brokerEmbeddedTest.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='moquette-bintray', root='http://dl.bintray.com/andsel/maven/')
-@Grab(group='io.moquette', module='moquette-broker', version='0.13.0')
+@Grab(group='io.moquette', module='moquette-broker', version='0.14.0-SNAPSHOT')
 
 import io.moquette.server.Server
 
